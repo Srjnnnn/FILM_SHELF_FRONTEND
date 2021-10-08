336e511b064f2a470be82355280613789b753f45
Sercan Uygur <s.uygur@azerion.com>
2021-10-08 23:49:18 +0300

    Add login and user creation functionalities

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -1,7 +1,116 @@
 import React from "react";
+import { gql, useLazyQuery, useMutation } from "@apollo/client";
+
+const GET_USER = gql`
+  query user($email: String!) {
+    user(email: $email) {
+      _id
+      isLoggedIn
+      sessionKeys
+      password
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+const LOGIN_USER = gql`
+  query login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      userMail
+      isLoggedIn
+      sessionKey
+    }
+  }
+`;
+
+const CREATE_USER = gql`
+  mutation createUser($email: String!, $password: String!) {
+    createUser(input: { email: $email, password: $password }) {
+      isLoggedIn
+      sessionKeys
+      email
+      _id
+      password
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 function LoginBar(props) {
-  console.log(props.result);
+  const [
+    checkUser,
+    { loading: checkUserLoading, error: checkUserError, data: userData },
+  ] = useLazyQuery(GET_USER, {
+    variables: { email: props.result[0] },
+  });
+
+  const [
+    logInUser,
+    { loading: checkLogLoading, error: checkLogError, data: logData },
+  ] = useLazyQuery(LOGIN_USER, {
+    variables: { email: props.result[0], password: props.result[1] },
+  });
+
+  const [
+    createUser,
+    { loading: checkCreateLoading, error: checkCreateError, data: createData },
+  ] = useMutation(CREATE_USER, {
+    variables: { email: props.result[0], password: props.result[1] },
+  });
+
+  if (checkUserLoading || checkLogLoading)
+    return (
+      <div>
+        <p>Loading ...</p>
+      </div>
+    );
+  if (checkUserError) return `Error! ${checkUserError}`;
+  if (checkLogError) return `Error! ${checkLogError}`;
+
+  if (
+    userData &&
+    !userData?.user &&
+    logData &&
+    logData?.login &&
+    !logData?.login.isLoggedIn
+  ) {
+    return (
+      <div>
+        <p>
+          Error! We can't find that user! Do you want to register with these
+          credentials?.
+        </p>
+        <button
+          className="bg-blue-500 border-2 rounded-lg py-3 mt-2 mx-2 px-6 font-bold text-white"
+          onClick={() => {
+            createUser();
+            logInUser();
+          }}
+        >
+          Yes
+        </button>
+        <button
+          className="bg-blue-500 border-2 rounded-lg py-3 mt-2 mx-2 px-6 font-bold text-white"
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          No
+        </button>
+      </div>
+    );
+  }
+  if (
+    userData &&
+    userData?.user &&
+    logData &&
+    logData?.login &&
+    !logData?.login.isLoggedIn
+  )
+    return `Error! The password is wrong! Please refresh the page and try again.`;
+
   return (
     <div>
       <div className="grid grid-cols-6">
@@ -22,8 +131,18 @@ function LoginBar(props) {
         />
       </div>
       <div className="text-center mt-4">
-      <button className="bg-blue-500 border-2 rounded-lg py-3 mt-2 mx-2 px-6 font-bold text-white">Submit</button>
-      <button className="bg-red-500 border-2 rounded-lg py-3 mt-2 mx-2 px-6 font-bold text-white">Log Out</button>
+        <button
+          className="bg-blue-500 border-2 rounded-lg py-3 mt-2 mx-2 px-6 font-bold text-white"
+          onClick={() => {
+            checkUser();
+            logInUser();
+          }}
+        >
+          Submit
+        </button>
+        <button className="bg-red-500 border-2 rounded-lg py-3 mt-2 mx-2 px-6 font-bold text-white">
+          Log Out
+        </button>
       </div>
     </div>
   );
